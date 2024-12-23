36b4b30d5ec57b50031b810d1ea2ed5c21f65ac8
Henrique Dias <heukirne@gmail.com>
2024-12-23 05:50:22 -0300

    Create db_config.py

---
+++ app/resources/db_config.py
@@ -0,0 +1,19 @@
+DATABASE_CONFIGS = {
+    "db1": {
+        "TYPE": "mysql",
+        "HOST": "host1",
+        "PORT": "port1",
+        "DATABASE": "db1",
+        "USER": "user1",
+        "PASS": "pass1"
+    },
+    "db2": {
+        "TYPE": "mysql",
+        "HOST": "host2",
+        "PORT": "port2",
+        "DATABASE": "db2",
+        "USER": "user2",
+        "PASS": "pass2"
+    },
+    # add as many as you need
+}
